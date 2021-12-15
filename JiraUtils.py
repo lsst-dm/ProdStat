1f8dcfed5f924abb0e3636fc17022810c3a4de75
kuropat <kuropat@fnal.gov>
2021-12-15 17:46:03 -0600

    Working on PEP 8 compliance of the code.

--- JiraUtils.py
+++ JiraUtils.py
@@ -1,16 +1,35 @@
 #!/usr/bin/env python
-"""The program to put comments and attachments to a Jira ticker"""
+# This file is part of ProdStat package.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import netrc
 from jira import JIRA
 import argparse
-import getpass
-import keyring
 import datetime
+
 
 class JiraUtils:
     def __init__(self):
         secrets = netrc.netrc()
         username, account, password = secrets.authenticators('lsstjira')
+        self.ajira = JIRA(options={'server': account}, basic_auth=(username, password))
 
     def get_login(self):
         """
@@ -31,7 +50,7 @@ class JiraUtils:
         issue_object = self.ajira.issue(ticket)
         return issue_object
 
-    def get_issueId(self, project, key):
+    def get_issue_id(self, project, key):
         """
         return issueID having project like 'Pre-Operetions' and key like 'PREOPS-910'
         :param project: project name, string
@@ -43,11 +62,12 @@ class JiraUtils:
             jql_str=jql_str
         )
         print("query=", query)
-        issueId = int(query[0].id)
-        print("Issue id=", issueId)
-        return issueId
+        issue_id = int(query[0].id)
+        print("Issue id=", issue_id)
+        return issue_id
 
-    def get_comments(self, issue):
+    @staticmethod
+    def get_comments(issue):
         """
         Creates dictionary of comments with keys = commentId
         :param issue: issue instance
@@ -63,7 +83,8 @@ class JiraUtils:
                     all_comments[comment["id"]] = comment["body"]
         return all_comments
 
-    def get_attachments(self,issue):
+    @staticmethod
+    def get_attachments(issue):
         """
         select all attachments in the issue and return dictionary of attachmentId: filename
         :param issue: issue instance
@@ -78,7 +99,8 @@ class JiraUtils:
                     print(attachment["id"], ' ', attachment["filename"])
         return all_attachments
 
-    def get_summary(self, issue):
+    @staticmethod
+    def get_summary(issue):
         """
         return summary of given issue
         :param issue: issue instance
@@ -89,16 +111,16 @@ class JiraUtils:
             print("Field:", field_name, "Value:", issue.raw['fields'][field_name])
         return summary
 
-    def add_attachment(self, jira, issue, attFile):
+    @staticmethod
+    def add_attachment(jira, issue, att_file):
         """
         Add attachment file to an issue
-        :param jira:
-        :param issue:
-        :param attFile:
+        :param jira: jira instance
+        :param issue: issue instance
+        :param att_file: attachment file
         :return:
         """
-        jira.add_attachment(issue=issue, attachment=attFile)
-
+        jira.add_attachment(issue=issue, attachment=att_file)
 
     """ creat issue with parameters in the issue dictionary
      that can look like:
@@ -109,89 +131,95 @@ class JiraUtils:
     "issuetype": issue_type_field
     }
     """
-    def create_issue(self, jira, issue_dict):
+
+    @staticmethod
+    def create_issue(jira, issue_dict):
         """
         Create an issue
-        :param jira:
-        :param issue_dict:
+        :param jira: jira instance
+        :param issue_dict: dictionary with issue fields
         :return: issue instance
         """
         new_issue = jira.create_issue(fields=issue_dict)
         return new_issue
 
-    def update_issue(self, issue, issue_dict):
+    @staticmethod
+    def update_issue(issue, issue_dict):
         """
         Update some or all issue fields
-        :param issue:
-        :param issue_dict:
+        :param issue: issue instance
+        :param issue_dict: dictionary with issue fields
         :return:
         """
         issue.update(fields=issue_dict)
 
-    def add_comment(self, issue, worklog):
+    @staticmethod
+    def add_comment(issue, work_log):
         """
         Add comment to an issue
         :param issue: issue instance
-        :param worklog: dictionary containing:
+        :param work_log: dictionary containing:
               'author': author name, string
               'created': timestamp, string
                'comment': comment body, string
         :return:
         """
-        issue.update(comment=worklog['comment'])
+        issue.update(comment=work_log['comment'])
 
-    def update_comment(self, jira, key, issueId, tokens, commentS):
+    def update_comment(self, jira, key, issue_id, tokens, comment_s):
         """
         Update comment replacing its body
         :param jira: api instance
         :param key:  issue key, string
         :param tokens: tokens to identify comment, list of strings
-        :param commentS: comment body, string
+        :param comment_s: comment body, string
+        :param issue_id:
         :return:
         """
         issue = self.get_issue(key)
         all_comments = self.get_comments(issue)
         if len(all_comments) != 0:
-            for id in all_comments:
-                commStr = self.get_comment(jira, issueId, id)
-                print('Id:', id, " comment:", commStr)
-                comment = jira.comment(key, id)  # with key and comment id
-                print("Found comment:",commStr)
+            for sid in all_comments:
+                comm_str = self.get_comment(jira, issue_id, sid)
+                print('Id:', sid, " comment:", comm_str)
+                comment = jira.comment(key, sid)  # with key and comment id
+                print("Found comment:", comm_str)
                 found = False
                 for token in tokens:
-                    if token in commStr:
+                    if token in comm_str:
                         found = True
                     else:
                         found = False
                         break
                 if found:
-                    comment.update(body=commentS)
+                    comment.update(body=comment_s)
         else:
-            worklog = dict()
-            worklog['author'] = 'Nikolay Kuropatkin'
-            tstamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            worklog['created'] = tstamp
-            worklog['comment'] = commentS
-            self.add_comment(issue, worklog)
+            work_log = dict()
+            work_log['author'] = 'Nikolay Kuropatkin'
+            t_stamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            work_log['created'] = t_stamp
+            work_log['comment'] = comment_s
+            self.add_comment(issue, work_log)
 
-    def get_comment(self, jira, issueId, commentId):
+    @staticmethod
+    def get_comment(jira, issue_id, comment_id):
         """
         Return comment body identified by issueID and commentId
         :param jira: jira API instance
-        :param issueId: issue id, string
-        :param commentId: comment id, string
+        :param issue_id: issue id, string
+        :param comment_id: comment id, string
         :return:
         """
-        comStr = jira.comment(int(issueId), int(commentId)).body
-        return comStr
+        com_str = jira.comment(int(issue_id), int(comment_id)).body
+        return com_str
 
-    def update_attachment(self, jira, issue, attFile):
+    def update_attachment(self, jira, issue, att_file):
         """
         to replace attachment in an issue we first delete one containing
         selected filename and then add a new one
         :param jira: jira API instance
         :param issue: issue instance
-        :param attFile: file /path/name, string
+        :param att_file: file /path/name, string
         :return:
         """
         attachments = issue.raw['fields']['attachment']
@@ -199,16 +227,17 @@ class JiraUtils:
         if len(attachments) != 0:
             for attachment in attachments:
                 print('attachment:', attachment['id'], ' ', attachment['filename'])
-                attId = attachment['id']
+                att_id = attachment['id']
                 filename = attachment['filename']
-                if filename in attFile:
+                if filename in att_file:
                     print(" Found attachment id ", attachment['id'])
-                    jira.delete_attachment(int(attId))
-                    self.add_attachment(jira, issue, attFile)
+                    jira.delete_attachment(int(att_id))
+                    self.add_attachment(jira, issue, att_file)
         else:
-            self.add_attachment(jira, issue, attFile)
+            self.add_attachment(jira, issue, att_file)
 
-    def get_description(self, issue):
+    @staticmethod
+    def get_description(issue):
         """
         read issue description
         :param issue: issue instance
@@ -216,6 +245,7 @@ class JiraUtils:
         """
         description = issue.raw['fields']['description']
         return description
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -268,20 +298,20 @@ def main():
         print("Failed to delete issue")
     """
     issue = ju.get_issue(ticket)
-    issueId = ju.get_issueId(project='DRP', key=ticket)
-    print("issueId=",issueId)
+    issue_id = ju.get_issue_id(project='DRP', key=ticket)
+    print("issueId=", issue_id)
     desc = ju.get_description(issue)
     print("desc:", desc)
     " Let's make attachment if not exists"
-    attFile = './table.html'
-    ju.update_attachment( jira, issue, attFile)
+    att_file = './table.html'
+    ju.update_attachment(jira, issue, att_file)
     print("issue fields attachment:", issue.fields.attachment)
     " Now create or update a comment"
-    commentS = """ The test comment for pandaStat and PREOPS-910"""
+    comment_s = """ The test comment for pandaStat and PREOPS-910"""
     tokens = ['pandaStat', 'PREOPS-910']
-    ju.update_comment(jira, ticket, issueId, tokens, commentS)
-    commentS = """ New comment for pandaStat and PREOPS-910"""
-    ju.update_comment(jira, ticket, issueId, tokens, commentS)
+    ju.update_comment(jira, ticket, issue_id, tokens, comment_s)
+    comment_s = """ New comment for pandaStat and PREOPS-910"""
+    ju.update_comment(jira, ticket, issue_id, tokens, comment_s)
     return
 
 
